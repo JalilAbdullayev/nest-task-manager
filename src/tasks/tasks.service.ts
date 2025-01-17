5d3d90eb542d982d2c18feed5e39a96f44615617
Jalil Abdullayev <abdullacelil00@gmail.com>
2025-01-17 09:12:55 +0400

    Validate get task by id
    - if task doesn't exist app will throw a not found exception with a message

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import Task, { TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import CreateTaskDto from './dto/create-task.dto';
@@ -27,7 +27,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task: Task) => task.id === id);
+    const found: Task = this.tasks.find((task: Task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found.`);
+    }
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
