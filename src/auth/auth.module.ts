d63e6ec344e288fd0ab0363ac06c715c851ef933
Jalil Abdullayev <abdullacelil00@gmail.com>
2025-01-19 18:20:43 +0400

    feat: sign up

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService, UsersRepository],
   controllers: [AuthController],
 })
