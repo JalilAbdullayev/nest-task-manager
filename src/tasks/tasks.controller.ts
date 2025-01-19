fe10e50495a6b1a2198114a74af80cf26363fb50
Jalil Abdullayev <abdullacelil00@gmail.com>
2025-01-19 20:47:10 +0400

    Create GetUser decorator
    - add guard to Tasks Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import TasksService from './tasks.service';
@@ -14,8 +15,10 @@ import Task from './task.entity';
 import CreateTaskDto from './dto/create-task.dto';
 import UpdateTaskStatusDto from './dto/update-task-status.dto';
 import GetTasksFilterDto from './dto/get-tasks-filter.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export default class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
