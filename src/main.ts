e5102c019f317c68922d82d295506785f3ecc517
Jalil Abdullayev <abdullacelil00@gmail.com>
2025-01-24 17:58:57 +0400

    TypeORM Configuration

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
   logger.log(`Application listening on port ${process.env.PORT ?? 3000}`);
 }
 
